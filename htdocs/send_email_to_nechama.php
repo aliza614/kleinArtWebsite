b838f15e19df90e033bcb3d0e6d0a1497f99d7e1
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-03 10:48:55 -0500

    made updates to form

--- htdocs/send_email_to_nechama.php
+++ htdocs/send_email_to_nechama.php
@@ -2,19 +2,19 @@
     <body>
         <?php
             //drafting the message
-            $fname=$_GET["fname"];
-            $lname=$_GET["lname"];
-            $email=$_GET["email"];
-            $message=$_GET["message"];
-            $full_message='from:'+$fname+' '+$lname+'\n\r'+$email+'\n\r'+$message;
+            $fname=$_POST["fname"];
+            //$lname=$_POST["lname"];
+            //$email=$_POST["email"];
+            //$message=$_POST["message"];
+            $message="testing"
+            //$full_message='from:'+$fname+' '+$lname+'\n\r'+$email+'\n\r'+$message;
             $subject='Someone just emailed you from your own site';
             //sending the message
-            $to="alizaasdf1234@gmail.com"
-            $headers="From: $email \r\n"
+            $to="alizaasdf1234@gmail.com";
+            $headers="From: $email \r\n";
             $headers .="Reply-To: $email \r\n";
-            mail($to,$subject,$full_message, $headers);
+mail($to,$subject,/*$full_message*/$message);//, $headers);
         ?>
-
         Your email has been sent.
     </body>
 </html>
