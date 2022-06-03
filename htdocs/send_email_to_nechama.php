7568d0cb20c69e83b0072632cc6e1277f369c571
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-03 02:07:29 -0500

    v1 of email form

--- htdocs/send_email_to_nechama.php
+++ htdocs/send_email_to_nechama.php
@@ -9,7 +9,10 @@
             $full_message='from:'+$fname+' '+$lname+'\n\r'+$email+'\n\r'+$message;
             $subject='Someone just emailed you from your own site';
             //sending the message
-            mail('alizaasdf1234@gmail.com',$subject,$full_message);
+            $to="alizaasdf1234@gmail.com"
+            $headers="From: $email \r\n"
+            $headers .="Reply-To: $email \r\n";
+            mail($to,$subject,$full_message, $headers);
         ?>
 
         Your email has been sent.
