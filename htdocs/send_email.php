c85954d4810ae4e4530e91b93c950cacc32ee56b
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-07 18:05:51 -0500

    adding that the form works

--- htdocs/send_email.php
+++ htdocs/send_email.php
@@ -1,4 +1,6 @@
 <?php
+$name="aliza";
+$message="testing 1234";
 $email_from = 'yourname@yourwebsite.com';
 
 $email_subject = "New Form submission";
@@ -16,5 +18,14 @@ $headers .= "Cc: someone@domain.com \r\n";
 
 $headers .= "Bcc: someoneelse@domain.com \r\n";
 
+// Please specify your Mail Server - Example: mail.example.com.
+ini_set("SMTP","smtp.gmail.com");//"smtp-relay.gmail.com");
+ini_set("mail.smtp.host", "smtp.googlemail.com");
+// Please specify an SMTP Number 25 and 8889 are valid SMTP Ports.
+ini_set("smtp_port",587);//25);
+
+// Please specify the return address to use
+ini_set('sendmail_from', 'alizaasd1234@gmail.com');
+//Must issue a STARTTLS command first.
 mail($to,$email_subject,$email_body,$headers);
 ?>
