7f5f9d9f0c3afac58c003dd4d6fbd7d700717579
aliza614 <47804976+aliza614@users.noreply.github.com>
2022-06-10 13:40:12 -0500

    Update mailUsingPHPMailer.php

--- mailUsingPHPMailer.php
+++ mailUsingPHPMailer.php
@@ -32,7 +32,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'irisjkleinsmtp@gmail.com';                     //SMTP username
-    $mail->Password   = 'Website613';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     //form results
